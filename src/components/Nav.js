6daed8d5a6bbc247911112bdee4c9e1bef2b3e1d
Ben Cook <benthomascook28@gmail.com>
2017-07-12 13:52:57 -0400

    Added header

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, DropdownButton, MenuItem, CollapsibleNav } from 'react-bootstrap';
+
+class NavBarHeader extends Component{
+	render(){
+		return(
+			<Navbar>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<a href="#">YouTube Plus</a>
+					</Navbar.Brand>
+				</Navbar.Header>
+					<Nav>
+						<NavItem eventKey={1} href="#">Sign In</NavItem>
+						<NavItem eventKey={2} href="#">Sign Up</NavItem>
+						<NavDropdown eventKey={3} title="Cool Stuff" id="basic-nav-dropdown">
+							<MenuItem eventKey={3.1}>Action</MenuItem>
+							<MenuItem eventKey={3.2}>Another Action</MenuItem>
+							<MenuItem eventKey={3.3}>A Third Action</MenuItem>
+							<MenuItem divider />
+							<MenuItem eventKey={3.3}>Separated Link</MenuItem>
+						</NavDropdown>
+					</Nav>
+			</Navbar>
+		);
+	}
+}
+export default NavBarHeader
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import NavBarHeader from './Nav';
 
-	export default class App extends Component {
-		render() {
-			return (
-				<div>React Hello World</div>
-				);
-		}
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<NavBarHeader />
+			</div>
+
+			);
 	}
+}
+
+export default App;
