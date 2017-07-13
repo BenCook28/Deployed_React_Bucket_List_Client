0009c52e04a9267801a8a3ff4367709b3ee1f6ef
Ben Cook <benthomascook28@gmail.com>
2017-07-13 15:14:13 -0400

    Attempted album cover challenge

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ const SELECT_BAND = 'SELECT_BAND';
 //selectBand is an action creator
 export function selectBand(band) {
 	console.log("You have selected:", band.name);
+	return band.image;
 	return {
 		type: 'SELECT_BAND',
 		payload: band
