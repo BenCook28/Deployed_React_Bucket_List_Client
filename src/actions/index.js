948fedccd50c984a51e6b66de65f7cd4dabe5781
Ben Cook <benthomascook28@gmail.com>
2017-07-14 10:35:26 -0400

    got input working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,24 @@
-//Action
-const SELECT_BAND = 'SELECT_BAND';
-//selectBand is an action creator
-export function selectBand(band) {
-	console.log("You have selected:", band.name);
-	return band.image;
+// //Action
+// const SELECT_BAND = 'SELECT_BAND';
+// //selectBand is an action creator
+// export function selectBand(band) {
+// 	console.log("You have selected:", band.name);
+// 	return band.image;
+// 	return {
+// 		type: 'SELECT_BAND',
+// 		payload: band
+// 	};
+// }
+import axios from 'axios';
+
+export const CREATE_POSTS = 'CREATE_POSTS';
+
+const ROOT_URL = 'http://rest.learncode.academy/api/youtubeplus';
+
+export function createPost(props) {
+	const request = axios.post(`${ROOT_URL}/posts`, props);
 	return {
-		type: 'SELECT_BAND',
-		payload: band
+		type: CREATE_POSTS,
+		payload: request
 	};
 }
